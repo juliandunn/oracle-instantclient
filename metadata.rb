79506849cdef4ec5be3ea56ec63cd7fe569c0617
Julian C. Dunn <jdunn@getchef.com>
2014-01-18 21:39:34 -0800

    Version bump to 1.0.2 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jdunn@getchef.com'
 license          'Apache 2.0'
 description      'Installs the Oracle Instant Client and SQL*Plus'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.0.2'
